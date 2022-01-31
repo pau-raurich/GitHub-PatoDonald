bd4f5bef650245984aede9deb568d2a85fa34000
Ivan <79653212+Ivan21907@users.noreply.github.com>
2022-01-31 16:53:26 +0100

    Commit amb les modificacions al main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,57 @@
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        String[] data = new String[num+1];
+
+        boolean igual = false;
+
+
+        for(int i=0; i <= num; i++)
+
+        {
+
+            data[i]= sc.nextLine();
+
+            for(int j=1; j < num; j++)
+
+            {
+
+                if(data[i].equals(data[j]))
+
+                {
+
+                    igual= false;
+
+                }else {
+
+                    igual=true;
+
+
+
+                }
+
+            }
+
+        }
+
+        if (igual != true) {
+
+            System.out.println("Si");
+
+        } else {
+
+            System.out.println("No");
+
+        }
+
+    }
+
 }
