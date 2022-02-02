4bf4843b0213ad789866cce664f787c6fd06c127
Ivan <79653212+Ivan21907@users.noreply.github.com>
2022-02-02 17:06:17 +0100

    Programa fet sense comentaris JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,56 @@
 import java.util.Scanner;
 
-public class Main {
-
-
-    public static void main(String[] args) {
-
+public class Main
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
+        //Variables
+        int numDates = sc.nextInt();
+        String[] dates;
+        int[] cumples;
+        boolean cumpleRepe;
 
-        int num = sc.nextInt();
-
-        String[] data = new String[num+1];
-
-        boolean igual = false;
-
-
-        for(int i=0; i <= num; i++)
-
+        //bucle per quan no hi ha cap cumple repetit
+        while(numDates != 0)
         {
+            sc.nextLine();
 
-            data[i]= sc.nextLine();
+            cumpleRepe = false;
+            dates = sc.nextLine().split(" ");
+            cumples = new int[numDates * 2];
 
-            for(int j=1; j < num; j++)
-
+            for(int i = 0; i < numDates; i++)
             {
-
-                if(data[i].equals(data[j]))
-
-                {
-
-                    igual= false;
-
-                }else {
-
-                    igual=true;
-
-
-
-                }
-
+                cumples[2 * i] = Integer.parseInt(dates[i].split("/")[0]);
+                cumples[2 * i + 1] = Integer.parseInt(dates[i].split("/")[1]);
             }
 
+            for(int i = 0; i < numDates - 1; i++)
+            {
+                for(int j = i + 1; j < numDates; j++)
+                {
+                    if(cumples[2 * i] == cumples[2 * j] &&
+                            cumples[2 * i + 1] == cumples[2 * j + 1])
+                    {
+                        cumpleRepe = true;
+                        break;
+                    }
+                }
+                //condició per dir si ha trobat una data repetida o no. En el cas que no hi hagi el programa acava.
+                // Si no el programa segueix fins que no es trobin dates que coincideixin
+                if(!cumpleRepe)
+                {
+                    System.out.println("No");
+                    break;
+                }
+                else
+                {
+                    System.out.println("Si");
+                    break;
+                }
+            }
+            numDates = sc.nextInt();
         }
-
-        if (igual != true) {
-
-            System.out.println("Si");
-
-        } else {
-
-            System.out.println("No");
-
-        }
-
     }
-
 }
